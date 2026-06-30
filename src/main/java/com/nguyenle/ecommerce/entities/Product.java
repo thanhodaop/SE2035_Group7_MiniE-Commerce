2a7146f28bfe5353767a64952061ed834934df43
thanhodaop <thanhodaop@gmail.com>
2026-06-30 15:13:40 +0700

    .

--- src/main/java/com/nguyenle/ecommerce/entities/Product.java
+++ src/main/java/com/nguyenle/ecommerce/entities/Product.java
@@ -1,4 +1,3 @@
-
 package com.nguyenle.ecommerce.entities;
 
 import jakarta.persistence.Column;
@@ -8,6 +7,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Transient;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -33,7 +33,24 @@ public class Product {
 
     private Integer quantity;
 
+    private Integer discountPercent;
+
     @ManyToOne
     @JoinColumn(name = "category_id")
     private Category category;
+
+    @Transient
+    public boolean isOnSale() {
+        return discountPercent != null && discountPercent > 0;
+    }
+
+    @Transient
+    public BigDecimal getDiscountedPrice() {
+        if (!isOnSale() || price == null) {
+            return price;
+        }
+        BigDecimal percent = BigDecimal.valueOf(discountPercent);
+        BigDecimal multiplier = BigDecimal.ONE.subtract(percent.divide(BigDecimal.valueOf(100)));
+        return price.multiply(multiplier);
+    }
 }
